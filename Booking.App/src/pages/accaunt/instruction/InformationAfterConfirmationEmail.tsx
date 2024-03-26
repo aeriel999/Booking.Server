e744ce07d0ca1074805313f8e2666875ecfe4cde
aeriel999 <ok4743@gmail.com>
2024-03-26 17:08:56 +0200

    Add ConfirmEmailPage, InformationPage and ErrorHandler.

---
+++ Booking.App/src/pages/accaunt/instruction/InformationAfterConfirmationEmail.tsx
@@ -0,0 +1,10 @@
+export  default function UserInformationPage(){
+
+    return(
+        <>
+            <h1>Instruction for User</h1>
+            <h1>Instruction for realtor</h1>
+
+        </>
+    )
+}
