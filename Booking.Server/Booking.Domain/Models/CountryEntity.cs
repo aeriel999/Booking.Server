acab75253d2a7d484895847ccfa04dfdb564128c
Bogdan Slonets <bogdan.slonets@gmail.com>
2024-04-02 16:15:16 +0300

    Add CountryTable

---
+++ Booking.Server/Booking.Domain/Models/CountryEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Models
+{
+    public class CountryEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? NameCountry { get; set; }
+    }
+}
--- Booking.Server/Booking.Infrastructure/Common/Persistence/BookingDbContext.cs
+++ Booking.Server/Booking.Infrastructure/Common/Persistence/BookingDbContext.cs
@@ -11,11 +11,11 @@ public class BookingDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid
 	public BookingDbContext() : base() { }
 	public BookingDbContext(DbContextOptions<BookingDbContext> options) : base(options) { }	
 	public DbSet<CategoryEntity> Category { get; set; }
-    //public DbSet<CountryEntity> Country { get; set; }
-    //public DbSet<TownEntity> Towns { get; set; }
-    //public DbSet<StreetEntity> Streets { get; set; }
-    //public DbSet<ImagesPostEntity> Images { get; set; }
-    protected override void OnModelCreating(ModelBuilder builder)
+	public DbSet<CountryEntity> Country { get; set; }
+	//public DbSet<TownEntity> Towns { get; set; }
+	//public DbSet<StreetEntity> Streets { get; set; }
+	//public DbSet<ImagesPostEntity> Images { get; set; }
+	protected override void OnModelCreating(ModelBuilder builder)
 	{
 		base.OnModelCreating(builder);
 	}
