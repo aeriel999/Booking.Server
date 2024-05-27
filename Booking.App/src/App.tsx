bba12c44eebdbcf7627b14436afbc5bdfab830f3
aeriel999 <ok4743@gmail.com>
2024-05-27 16:04:53 +0300

    Edit Message view

--- Booking.App/src/App.tsx
+++ Booking.App/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import NotFound from "./pages/errors/NotFound.tsx";
 import RealtorRegisterPage from "./pages/accaunt/register/RealtorRegisterPage.tsx";
 import SignInPage from "./pages/accaunt/login/SignIn.tsx";
-import {useAppSelector} from "./hooks/redux";
+import { useAppSelector } from "./hooks/redux";
 import DashboardLayout from "./containers/dashboard/layouts/_DashboardLayot.tsx";
 import ChangeEmailPage from "./pages/accaunt/change-email/ChangeEmailPage.tsx";
 import UserRegisterPage from "./pages/accaunt/register/UserRegisterPage.tsx";
@@ -19,13 +19,13 @@ import ResetPasswordPage from "./pages/accaunt/reset-password/ResetPasswordPage.
 import ReConfirmEmailPage from "./pages/reconfirm-email/ReConfirmEmailPage.tsx";
 //import ChatRoom  from "./containers/common/ChatRoom.tsx";
 import ClientDashboardLayout from "./containers/client/layouts/_ClientDashboardLayout.tsx";
-import {AddNewPost} from "./containers/dashboard/AddNewPost.tsx";
+import { AddNewPost } from "./containers/dashboard/AddNewPost.tsx";
 import ListOfPostPage from "./containers/client/ListOfPostPage.tsx";
 import AnonymousDashboardLayout from "./containers/anonymous/layouts/AnonymousDashboardLayout.tsx";
+import {  MessageRight } from "./components/chat/Message.tsx";
+//import { MessageLeft } from "./components/chat/Message.tsx";
 
-
-const App : React.FC = () => {
-
+const App: React.FC = () => {
     // const [connection, setConnection] = useState<any>({});
     // const [messages, setMessages] = useState<IMessage[]>([]);
     //
@@ -79,86 +79,122 @@ const App : React.FC = () => {
     //     }
     // }
 
-    const {isLogin, user} = useAppSelector(state => state.account);
+    const { isLogin, user } = useAppSelector((state) => state.account);
 
-   const role = () =>{
-       console.log("user", user)
-       console.log("isLogin", isLogin)
+    const role = () => {
+        console.log("user", user);
+        console.log("isLogin", isLogin);
 
-       if (user?.role.toLowerCase().includes('realtor'))
-       {
-
-          return 'realtor'
-       }else if(user?.role.toLowerCase().includes('user'))
-       {
-           return 'user'
-       }else if(user?.role.toLowerCase().includes('admin'))
-       {
-           return 'admin'
-       }
-   }
+        if (user?.role.toLowerCase().includes("realtor")) {
+            return "realtor";
+        } else if (user?.role.toLowerCase().includes("user")) {
+            return "user";
+        } else if (user?.role.toLowerCase().includes("admin")) {
+            return "admin";
+        }
+    };
 
     return (
         <Routes>
             {isLogin && (
-               <>
-                   <Route path="/instructions" element={<InformationAfterConfirmationEmail/>} />
+                <>
+                    <Route
+                        path="/instructions"
+                        element={<InformationAfterConfirmationEmail />}
+                    />
 
-                   {role() === "realtor" && (
-                       <Route path="/dashboard"  element={<DashboardLayout/>} >
-                           <Route index element={<RealtorProfilePage />} />
-                           <Route path="/dashboard/profile" element={<RealtorProfilePage/>} />
-                           <Route path="/dashboard/profile/edit" element={<RealtorProfileEditPage/>} />
-                           <Route path="/dashboard/profile/change-password" element={<ChangePasswordPage/>} />
-                           <Route path="/dashboard/post/add" element={<AddNewPost/>} />
+                    {role() === "realtor" && (
+                        <Route path="/dashboard" element={<DashboardLayout />}>
+                            <Route index element={<RealtorProfilePage />} />
+                            <Route
+                                path="/dashboard/profile"
+                                element={<RealtorProfilePage />}
+                            />
+                            <Route
+                                path="/dashboard/profile/edit"
+                                element={<RealtorProfileEditPage />}
+                            />
+                            <Route
+                                path="/dashboard/profile/change-password"
+                                element={<ChangePasswordPage />}
+                            />
+                            <Route
+                                path="/dashboard/post/add"
+                                element={<AddNewPost />}
+                            />
+                        </Route>
+                    )}
 
-                       </Route>
-                   )}
+                    {role() === "user" && (
+                        <Route path="/" element={<ClientDashboardLayout />}>
+                            <Route index element={<UserProfilePage />} />
+                            <Route
+                                path="/profile"
+                                element={<UserProfilePage />}
+                            />
+                            <Route path="/posts" element={<ListOfPostPage />} />
+                            {/*<Route path="/dashboard/chat/:postId" element={<ChatRoom/>} />*/}
+                        </Route>
+                    )}
 
-                   {role() === "user" && (
-                       <Route path="/"  element={<ClientDashboardLayout/>} >
-                           <Route index element={<UserProfilePage />} />
-                           <Route path="/profile" element={<UserProfilePage/>} />
-                           <Route path="/posts" element={<ListOfPostPage/>} />
-                           {/*<Route path="/dashboard/chat/:postId" element={<ChatRoom/>} />*/}
-                       </Route>
-                   )}
-
-               </>
+                    {/* <Route path="test" element={<MessageLeft />} /> */}
+                    <Route path="test2" element={<MessageRight />} />
+                </>
             )}
-            <Route/>
-
+            <Route />
             {/*{!connection*/}
             {/*    ? <Route path="/test" element={<WaitingRoom joinChatRoom={joinChatRoom} />} />*/}
             {/*    : <Route path="/test" element={<ChatRoom messages={messages} sendMSG={sendMSG} />} />}*/}
-
             {/* <Route path="*" element={<SignInPage />} /> */}
-            
-            <Route path="/"  element={<AnonymousDashboardLayout/>} >
+            <Route path="/" element={<AnonymousDashboardLayout />}>
                 <Route index element={<ListOfPostPage />} />
-
             </Route>
-
-
-            <Route path="/dashboard/profile" element={<SignInPage/>} />
-            <Route path="/dashboard/profile/edit" element={<SignInPage/>} />
-            <Route path="/dashboard/profile/change-password" element={<SignInPage/>} />
-           // <Route path="*" element={<NotFound/>} />
-            <Route path="/authentication/user-register" element={<UserRegisterPage/>} />
-            <Route path="/authentication/register-information/:email" element={<RegisterInformationPage/>} />
-            <Route path="/authentication/confirm-email/:userId/:token"  element={<ConfirmEmailPage/>} />
-            <Route path="/authentication/realtor-register" element={<RealtorRegisterPage/>} />
-            <Route path="/authentication/login" element={<SignInPage/>} />
-            <Route path="/authentication/forgot-password" element={<ForgotPasswordPage/>} />
-            <Route path="/authentication/forgot-password-information/:email" element={<ForgotPasswordInformationPage/>} />
-            <Route path="/authentication/reset-password/:email/:token"  element={<ResetPasswordPage/>} />
-            <Route path="/authentication/change-email/:userId/:email/:token" element={<ChangeEmailPage/>} />
-            <Route path="/authentication/reconfirm-email" element={<ReConfirmEmailPage/>} />
-
+            <Route path="/dashboard/profile" element={<SignInPage />} />
+            <Route path="/dashboard/profile/edit" element={<SignInPage />} />
+            <Route
+                path="/dashboard/profile/change-password"
+                element={<SignInPage />}
+            />
+            // <Route path="*" element={<NotFound />} />
+            <Route
+                path="/authentication/user-register"
+                element={<UserRegisterPage />}
+            />
+            <Route
+                path="/authentication/register-information/:email"
+                element={<RegisterInformationPage />}
+            />
+            <Route
+                path="/authentication/confirm-email/:userId/:token"
+                element={<ConfirmEmailPage />}
+            />
+            <Route
+                path="/authentication/realtor-register"
+                element={<RealtorRegisterPage />}
+            />
+            <Route path="/authentication/login" element={<SignInPage />} />
+            <Route
+                path="/authentication/forgot-password"
+                element={<ForgotPasswordPage />}
+            />
+            <Route
+                path="/authentication/forgot-password-information/:email"
+                element={<ForgotPasswordInformationPage />}
+            />
+            <Route
+                path="/authentication/reset-password/:email/:token"
+                element={<ResetPasswordPage />}
+            />
+            <Route
+                path="/authentication/change-email/:userId/:email/:token"
+                element={<ChangeEmailPage />}
+            />
+            <Route
+                path="/authentication/reconfirm-email"
+                element={<ReConfirmEmailPage />}
+            />
         </Routes>
     );
-}
+};
 
-export default App
-
-
+export default App;
